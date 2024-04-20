8b9c09fe3e0bba57e93e237ae9d2ec49823ba665
Devika kumari <devika21@navgurukul.org>
2024-04-20 19:48:10 +0530

    I have changed the variables' names

--- app/Search_bar.js
+++ app/Search_bar.js
@@ -2,35 +2,38 @@
 
 import React, { useState } from "react";
 import "./search_bar.css";
+import { articles } from "./data";
 
-function SearchBar() {
+function Search_bar() {
     const [input, setInput] = useState("");
     
-    const handleSearch= () =>{
+    const handleSearch = () => {
+        const new_articles = [];
+
         const apiKey = '69c7f892cd89443f8cd4e75d3aa7e2b4';
-        const articlesArray=[]
         fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=${apiKey}`)
-            .then(response =>response.json())
-            .then((response)=>{
-                if (!response.ok) {
-                    const articles =response.articles;
-                    for (let i = 0; i < articles.length; i++) {
-                        const article = articles[i];
-                        const articleObject = {
-                        image: article.urlToImage,
-                        title: article.title,
-                        description: article.description,
-                        url: article.url
-                        };
-                        articlesArray.push(articleObject);
+            .then(response => response.json())
+            .then((data) => {
+               if (!data.ok) {
+                    const search_articles = data.articles;
+                    for (let i=0; i<search_articles.length; i++) {
+                        const search_article = search_articles[i];
+                        const new_article = {
+                            image: search_article.urlToImage,
+                            title: search_article.title,
+                            description: search_article.description,
+                            articleUrl: search_article.url
+                        }
+                        new_articles.push(new_article);
                     }
-                    console.log(articlesArray)
+                    console.log(new_articles);
                 }
             })
             .catch(error => {
                 console.error('There was a problem with your fetch operation:', error);
-            })
-    }
+            });
+    };
+    
     return (
         <div className="container">
             <div className="searchbar-row">
@@ -50,5 +53,5 @@ function SearchBar() {
         </div>
     );
 }
-export default SearchBar;
+export default Search_bar;
 
